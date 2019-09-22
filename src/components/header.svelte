d099ba5869cb77dc076868e9ba83e09fc45cdf34
Charlie Waite <cw5790@gmail.com>
2019-09-22 20:41:09 +0100

    Add header component for navigation

---
+++ src/components/header.svelte
@@ -0,0 +1,46 @@
+<style>
+  .header-wrapper {
+    position: fixed;
+    display: flex;
+    background: #fff;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.19);
+    height: 65px;
+    justify-content: space-between;
+    top: 0;
+    width: 100%;
+  }
+
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: auto;
+    width: 100%;
+
+    max-width: 1200px;
+  }
+
+  nav {
+    display: flex;
+    align-items: center;
+  }
+
+  .nav-item {
+    margin-right: 10px;
+  }
+
+  h2 {
+    margin: 0;
+  }
+</style>
+
+<div class="header-wrapper">
+  <div class="header">
+    <h2>F1 Stats</h2>
+    <nav>
+      <div class="nav-item">Stats</div>
+      <div class="nav-item">Races</div>
+      <div class="nav-item">Qualifying</div>
+    </nav>
+  </div>
+</div>
