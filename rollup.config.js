9ae1a3464942d56b776d4137ecb40469afa1169c
Charlie Waite <cw5790@gmail.com>
2019-09-23 22:18:22 +0100

    Added SASS support

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
+import { sass } from "svelte-preprocess-sass";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -19,6 +20,9 @@ export default {
       dev: !production,
       css: css => {
         css.write("public/bundle.css");
+      },
+      preprocess: {
+        style: sass()
       }
     }),
     resolve({
