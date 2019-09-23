d58ca2da14459cdcb149eaa957ad2ebb49198429
Charlie Waite <cw5790@gmail.com>
2019-09-23 22:18:34 +0100

    Add routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,29 @@
 <script>
+  import { Router, Route } from "svelte-routing";
   import Header from "./components/header.svelte";
-  import Qualifying from "./components/qualifying/qualifying.svelte";
+  import Qualifying from "./pages/qualifying-page.svelte";
+
+  export let url = "";
 </script>
 
 <style>
   main {
-    padding-top: 50px;
+    padding-top: 130px;
     text-align: center;
     margin: auto;
     max-width: 1200px;
   }
 </style>
 
-<div>
+<Router {url}>
   <Header />
 
   <main>
-    <Qualifying />
+    <Route path="/">
+      <Qualifying />
+    </Route>
+    <Route path="/about">
+      <div>Abouty McAboutyface</div>
+    </Route>
   </main>
-</div>
+</Router>
