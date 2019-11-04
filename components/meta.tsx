ebd7fdf916640b7aa0022082b0d4d2e900f6db0b
Charlie Waite <cw5790@gmail.com>
2019-11-04 22:24:35 +0000

    Add metadata to head

---
+++ components/meta.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Meta = (): JSX.Element => (
+  <Head>
+    <meta name="viewport" content="width=device-width initial-scale=1" />
+    <meta charSet="utf-8" />
+    <meta name="theme-color" content="#e10600" />
+    <link rel="stylesheet" href="/public/app.css" />
+    <title>F1 Stats</title>
+  </Head>
+);
+
+export default Meta;
