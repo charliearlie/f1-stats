452e51f16bced9c100baca568440da9b5928122f
Charlie Waite <cw5790@gmail.com>
2019-11-04 22:22:57 +0000

    Add date service

---
+++ services/date-service.js
@@ -0,0 +1,20 @@
+import dayjs from "dayjs";
+export default class DateService {
+  getRaceWeekendDates(date) {
+    return `${dayjs(date)
+      .subtract(2, "day")
+      .format("DD")} - ${dayjs(date).format("DD")} ${dayjs(date).format(
+      "MMM"
+    )}`;
+  }
+
+  getRaceWeekendDatesObject(date) {
+    return {
+      days: `${dayjs(date)
+        .subtract(2, "day")
+        .format("DD")}-${dayjs(date).format("DD")}`,
+      month: `${dayjs(date).format("MMM")}`,
+      year: `${dayjs(date).format("YYYY")}`
+    };
+  }
+}
