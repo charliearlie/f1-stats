8b209fefff067a3e7fbc6b7758ddc84c8f056b46
Charlie Waite <cw5790@gmail.com>
2019-11-06 22:25:18 +0000

    Add an API call for seasons

--- pages/qualifying.js
+++ pages/qualifying.js
@@ -3,6 +3,8 @@ import { makeStyles, createStyles } from "@material-ui/styles";
 import QualifyingResult from "../components/qualifying/qualifying-result";
 import DateService from "../services/date-service";
 import ApiService from "../services/api-service";
+
+import Dropdown from "../components/common/dropdown";
 import Link from "next/link";
 
 const useStyles = makeStyles(theme =>
@@ -30,8 +32,15 @@ const useStyles = makeStyles(theme =>
     },
     main: {
       margin: "0 auto",
-      marginTop: "80px",
+      marginTop: "120px",
       maxWidth: 1280
+    },
+    header: {
+      display: "flex",
+      justifyContent: "space-between",
+      "& h1": {
+        marginTop: 0
+      }
     }
   })
 );
@@ -40,16 +49,26 @@ function QualifyingPage(props) {
   const api = new ApiService();
   const dates = new DateService();
   const [qualiData, setQualiData] = useState(null);
+  const [seasons, setSeasons] = useState(null);
   const styles = useStyles(props);
 
   useEffect(() => {
     fetchQualiData();
+    fetchListOfSeasons();
   }, []);
 
-  const fetchQualiData = async () => {
-    const data = await api.getSeasonQualifyingResults();
-    console.log(data);
+  const fetchQualiData = async season => {
+    const data = await api.getSeasonQualifyingResults(season);
     setQualiData(data);
+  };
+
+  const fetchListOfSeasons = async () => {
+    const seasonData = await api.getSeasons();
+    setSeasons(seasonData);
+  };
+
+  const changeQualiYear = season => {
+    fetchQualiData(season);
   };
 
   return (
@@ -58,10 +77,20 @@ function QualifyingPage(props) {
         <div className={styles.container}>
           {/* <Qualifying qualiData={qualiData} /> */}
           <div className={styles.primary}>
-            <h1>2019 Season qualifying results</h1>
+            <div className={styles.header}>
+              <h1>{qualiData && qualiData.year} Season qualifying results</h1>
+              <Dropdown
+                afterChange={changeQualiYear}
+                options={seasons}
+                startingOption={69}
+              />
+            </div>
+
             <div className={styles.seasonResults}>
               {qualiData &&
-                qualiData.map(event => <QualifyingResult qualiData={event} />)}
+                qualiData.results.map(event => (
+                  <QualifyingResult qualiData={event} />
+                ))}
             </div>
           </div>
           <div></div>
--- services/api-service.js
+++ services/api-service.js
@@ -64,9 +64,10 @@ export default class ApiService {
     }
   }
 
-  async getSeasonQualifyingResults() {
+  async getSeasonQualifyingResults(season) {
+    const year = season || "current";
     const res = await fetch(
-      `https://ergast.com/api/f1/current/qualifying/1.json`
+      `https://ergast.com/api/f1/${year}/qualifying/1.json`
     );
 
     const data = await res.json();
@@ -75,15 +76,34 @@ export default class ApiService {
       if (data.MRData) {
         const races = data.MRData.RaceTable.Races;
 
-        return races.map(race => ({
-          circuit: race.Circuit.circuitName,
-          country: race.Circuit.Location.country,
-          date: race.date,
-          fastestLap: race.QualifyingResults[0],
-          raceName: race.raceName,
-          round: race.round,
-          season: race.season
-        }));
+        return {
+          year: data.MRData.RaceTable.season,
+          results: races.map(race => ({
+            circuit: race.Circuit.circuitName,
+            country: race.Circuit.Location.country,
+            date: race.date,
+            fastestLap: race.QualifyingResults[0],
+            raceName: race.raceName,
+            round: race.round,
+            season: race.season
+          }))
+        };
+      }
+    } else {
+      return "Error";
+    }
+  }
+
+  async getSeasons() {
+    const res = await fetch("https://ergast.com/api/f1/seasons.json?limit=100");
+
+    const data = await res.json();
+
+    if (res.ok) {
+      if (data.MRData) {
+        const seasons = data.MRData.SeasonTable.Seasons;
+
+        return seasons.map(season => season.season);
       }
     } else {
       return "Error";
