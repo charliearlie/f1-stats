7d4b3e8fe498275eee2e4cf4bd94a69ae142f412
charliearlie <cw5790@gmail.com>
2023-03-19 19:18:26 +0000

    Remove CHakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,13 @@
 import type { AppProps } from "next/app";
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
 import Page from "../components/page";
-import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Page>
-        <h1>Where the header will be</h1>
-        <Component {...pageProps} />
-      </Page>
-    </ChakraProvider>
+    <Page>
+      <h1>Where the header will be</h1>
+      <Component {...pageProps} />
+    </Page>
   );
 }
 
